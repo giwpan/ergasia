f47cb5c6880bf7a73e1957db65a98711e3c64287
giwpan <92867073+giwpan@users.noreply.github.com>
2023-01-09 20:25:32 +0200

    Add files via upload

---
+++ School.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.school;
+
+/**
+ *
+ * @author giwrg
+ */
+public class School {
+
+    public static void main(String[] args) {
+        System.out.println("School");
+    }
+}
+class Teacher {
+  public static void main(String[] args) {
+      System.out.println("Teacher");
+    }
+} 
+class Teacher 2 {
+  public static void main(String[] args) {
+      System.out.println("Teacher 2");
+  }
+}
+class Teacher 3 {
+  public static void main(String[] args) {
+      System.out.println("Teacher 3");
+  }
+}
+class Teacher 4 {
+  public static void main(String[] args) {
+      System.out.println("Teacher 4");
+  }
+}
+class Teacher 5 {
+  public static void main(String[] args) {
+      System.out.println("Teacher 5");
+  }
+}
+class Student {
+  public static void main(String[] args) {
+      System.out.println("Student");
+  }
+}
+class Subject 1 {
+  public static void main(String[] args) {
+      System.out.println("Subject 1");
+  }  
+}
+class Subject 2 {
+  public static void main(String[] args) {
+      System.out.println("Subject 2");
+  }  
+}
+class Subject 3 {
+  public static void main(String[] args) {
+      System.out.println("Subject 3");  
+  }
+}
+class Subject 4 {
+  public static void main(String[] args) {
+      System.out.println("Subject 4"); 
+  }
+}
+class Subject 5 {
+  public static void main(String[] args) {
+      System.out.println("Subject 5"); 
+  }
+}
